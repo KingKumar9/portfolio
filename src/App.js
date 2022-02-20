fa6a6d2466d3882f635c2a66550ae5b7a16b42f7
Rajat Kumar <rajatk12199@gmail.com>
2022-02-21 08:53:20 +1100

    body development and header reconstruction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Container, Row, Nav } from 'react-bootstrap';
+import { Container, Col, Row, Nav } from 'react-bootstrap';
 
 class App extends React.Component {
   render() {
     return (
       <Container fluid>
-        <Row className="nav">
-          <Nav variant="pills" className="justify-content-center" defaultActiveKey="link-1">
+
+        <Row className="header" sm={12}>
+          <Col className="title">
+            <h1>rajat kumar</h1>
+          </Col>
+
+          <Nav as={Col} variant="pills" className="justify-content-center" defaultActiveKey="link-1">
             <Nav.Item>
-              <Nav.Link eventKey="link-1">home</Nav.Link>
+              <Nav.Link eventKey="link-1">Home</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link-2">projects</Nav.Link>
+              <Nav.Link eventKey="link-2">Projects</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link-3">experience</Nav.Link>
+              <Nav.Link eventKey="link-3">Experience</Nav.Link>
             </Nav.Item>
           </Nav>
         </Row>
+
+        <Row className="body" sm={12}>
+        
+        </Row>
+
       </Container>
     )
   }
