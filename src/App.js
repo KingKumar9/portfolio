e75afab234b04eca7633b525c9d4a97240d0304b
Rajat Kumar <rajatk12199@gmail.com>
2022-02-22 06:41:05 +1100

    Complete overhaul on layout and structure of website including removing bootstrap from project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Container, Col, Row, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faReact, faFontAwesome } from '@fortawesome/free-brands-svg-icons';
+import { faArrowAltCircleDown } from '@fortawesome/free-solid-svg-icons';
 
 class App extends React.Component {
   render() {
     return (
-      <Container fluid>
-
-        <Row className="header" sm={12}>
-          <Col className="title">
-            <h1>rajat kumar</h1>
-          </Col>
-
-          <Nav as={Col} variant="pills" className="justify-content-center" defaultActiveKey="link-1">
-            <Nav.Item>
-              <Nav.Link eventKey="link-1">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="link-2">Projects</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="link-3">Experience</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Row>
-
-        <Row className="body" sm={12}>
+      <div className='container'>
         
-        </Row>
+        <div className='header'>
+          <div className='name'>
+            <h1>rajat kumar</h1>
+          </div>
+          <div className='links'>
+            <button>Home</button>
+            <button>Projects</button>
+            <button>Experience</button>
+          </div>
+        </div>
 
-      </Container>
+        <div className='body'>
+          <div className='card'></div>
+          <div className='text'></div>
+        </div>
+
+        <div className='footer'>
+          <div>
+            <a className='cv' href='rajatkumar-cv.docx' download>
+              <button><FontAwesomeIcon icon={ faArrowAltCircleDown } /> download cv</button>
+            </a>
+          </div>
+          <div>
+            <h4 className='footer-text'>Made with React <FontAwesomeIcon icon={ faReact } /> & Font Awesome <FontAwesomeIcon icon={ faFontAwesome } /></h4>
+          </div>
+          <div className='profiles'>
+            <a href='https://github.com/KingKumar9'>
+              <FontAwesomeIcon icon={ faGithub } />
+            </a>
+            <a href='https://www.linkedin.com/in/rajat-kumar-66ba72175/'>
+              <FontAwesomeIcon icon={ faLinkedin } />
+            </a>
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
